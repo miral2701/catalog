3a131d603a06f7d97a3113a88cb1e4f2f0d5619f
Mira Madani <miramadani2701@gmail.com>
2023-11-09 02:02:45 +0200

    сделано

--- OOP/Множественное наследование.cpp
+++ OOP/Множественное наследование.cpp
@@ -58,7 +58,38 @@ public:
 		rector = a;
 	}
 };
-int main() {
 
+class Student:public University, public Human
+{
+	int arr[10];
+	int year;
+	string faculty;
+
+public:
+	Student(int* ar, int y, string faculty1, string a1, int b1, string c, string a, int b) :University(a1, b1, c), Human(a, b)
+	{
+		for (int i = 0; i < 10; i++) {
+			arr[i] = ar[i];
+		}
+		year = y;
+		faculty = faculty1;
+	}
+	int getAge() {
+		return age;
+	}
+	string getName() {
+		return name;
+	}
+
+	void setAge(int a) {
+		age = a;
+	}
+	void setName(string a) {
+		name = a;
+	}
+
+};
+
+int main() {
 
 }
